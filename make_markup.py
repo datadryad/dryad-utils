f6873ffc4782130f5e6e02db0859fc7c5238142e
Dan Leehr <dan.leehr@nescent.org>
2014-12-01 11:19:27 -0500

    Update make_markup to take arguments and print instructions

--- make_markup.py
+++ make_markup.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+
 """
 make_markup.py
 ~~~~~~~~~~~~
@@ -10,77 +11,30 @@ Creates markup needed when adding journals to recently integrated rotation or to
 """
 __author__ = 'dan'
 
+import argparse
+import sys
+
 def render_itemsummary(journal_name, journal_url, cover_image):
     markup = "\
-                    <xsl:when test='$journal-name = \"" + journal_name + "\"'>\n\
-                        <a target=\"_blank\">\n\
-                            <xsl:attribute name=\"href\">\n\
-                                <xsl:choose>\n\
-                                    <xsl:when test=\"contains($article-doi,'doi:')\">\n\
-                                        <xsl:value-of\n\
-                                                select=\"concat('http://dx.doi.org/', substring-after($article-doi, 'doi:'))\"/>\n\
-                                    </xsl:when>\n\
-                                    <xsl:otherwise>\n\
-                                        <xsl:value-of\n\
-                                                select=\"string('" + journal_url + "')\"/>\n\
-                                    </xsl:otherwise>\n\
-                                </xsl:choose>\n\
-                            </xsl:attribute>\n\
-                            <img class=\"pub-cover\" id=\"journal-logo\" src=\"/themes/Dryad/images/coverimages/" + cover_image + "\"\n\
-                                 alt=\"" + journal_name +" cover\"/>\n\
-                        </a>\n\
-                    </xsl:when>"
+                <xsl:when test='$journal-name = \"" + journal_name + "\"'>\n\
+                    <a target=\"_blank\">\n\
+                        <xsl:attribute name=\"href\">\n\
+                            <xsl:choose>\n\
+                                <xsl:when test=\"contains($article-doi,'doi:')\">\n\
+                                    <xsl:value-of\n\
+                                            select=\"concat('http://dx.doi.org/', substring-after($article-doi, 'doi:'))\"/>\n\
+                                </xsl:when>\n\
+                                <xsl:otherwise>\n\
+                                    <xsl:value-of\n\
+                                            select=\"string('" + journal_url + "')\"/>\n\
+                                </xsl:otherwise>\n\
+                            </xsl:choose>\n\
+                        </xsl:attribute>\n\
+                        <img class=\"pub-cover\" id=\"journal-logo\" src=\"/themes/Dryad/images/coverimages/" + cover_image + "\"\n\
+                             alt=\"" + journal_name +" cover\"/>\n\
+                    </a>\n\
+                </xsl:when>"
     return markup
-
-
-gms_journals = (
-    ("GMS German Plastic, Reconstructive and Aesthetic Surgery", "http://www.egms.de/dynamic/de/journals/gpras/index.htm", "logo_gpras.png"),
-    ("GMS Infectious Diseases", "http://www.egms.de/dynamic/en/journals/id/index.htm", "logo_id.png"),
-    ("GMS Interdisciplinary Plastic and Reconstructive Surgery DGPW", "http://www.egms.de/dynamic/en/journals/iprs/", "logo_iprs_klein.png"),
-    ("GMS Onkologische Rehabilitation und Sozialmedizin","http://www.egms.de/dynamic/en/journals/ors/index.htm", "logo_dgho.png"),
-    ("GMS Zeitschrift für Medizinische Ausbildung","http://www.egms.de/dynamic/en/journals/zma/index.htm", "logo_gma_klein.png"),
-    ("GMS Zeitschrift zur Förderung der Qualitätssicherung in medizinischen Laboratorien", "http://www.egms.de/dynamic/en/journals/lab/", "logo_lab.png")
-)
-
-jan27_journals = (
-    ("Journal of Hymenoptera Research","http://www.pensoft.net/journals/jhr/","JHymenopRes.png"),
-    ("Subterranean Biology","http://www.pensoft.net/journals/subtbiol/","SubterraneanBiol.png"),
-)
-
-feb20_journals = (
-    ("Deutsche Entomologische Zeitschrift","http://www.pensoft.net/journals/dez/", "DEZ.png"),
-)
-
-bmc_journals = (
-    ("BMC Ecology", "http://www.biomedcentral.com/bmcecol", "BMCEcology.png"),
-    ("BMC Evolutionary Biology", "http://www.biomedcentral.com/bmcevolbiol", "BMCEvolBiology.png"),
-)
-
-apr2_journals = (
-    ("Proceedings of the Royal Society B","http://rspb.royalsocietypublishing.org","ProceedingsB.png"),
-    ("Scientific Data","http://www.nature.com/scientificdata/","ScientificData.png"),
-)
-
-elsevier_covers = (
-    ("Basic and Applied Ecology","http://www.elsevier.com/locate/baae","baae.jpg"),
-    ("Perspectives in Plant Ecology, Evolution and Systematics","http://www.elsevier.com/locate/ppees","ppees.jpg"),
-    ("Flora - Morphology, Distribution, Functional Ecology of Plants","http://www.elsevier.com/locate/flora","flora.jpg"),
-)
-
-sep23_journals = (
-    ("Ecological Applications", "http://www.esajournals.org/loi/ecap", "ecoApp.png"),
-    ("Royal Society Open Science","http://royalsocietypublishing.org/royal-society-open-science","rsos.png"),
-)
-
-plos_journals = (
-    ("PLOS Biology", "http://journals.plos.org/plosbiology/","plosbiology.jpg"),
-    ("PLOS Computational Biology", "http://journals.plos.org/ploscompbiol/","ploscompbiol.jpg"),
-    ("PLOS Genetics", "http://journals.plos.org/plosgenetics/","plosgenetics.jpg"),
-    ("PLOS Medicine", "http://journals.plos.org/plosmedicine/","plosmedicine.jpg"),
-    ("PLOS Neglected Tropical Diseases", "http://journals.plos.org/plosntds/","plosntds.jpg"),
-    ("PLOS ONE", "http://www.plosone.org","plosone.jpg"),
-    ("PLOS Pathogens", "http://journals.plos.org/plospathogens/","plospath.jpg"),
-)
 
 def render_recentlyintegrated(journal_name, cover_image):
     escaped = journal_name.replace(' ', '%5C+')
@@ -89,16 +43,38 @@ def render_recentlyintegrated(journal_name, cover_image):
     markup = "\
         <!-- " + journal_name + " -->\n\
 		  <a class=\"single-image-link\" href=\"/discover?field=prism.publicationName_filter&amp;query=&amp;fq=prism.publicationName_filter%3A" + lowercased + separator + escaped + "\">\
-    <img class=\"pub-cover\" src=\"/themes/Mirage/images/recentlyIntegrated-" + cover_image + "\" alt=\"" + journal_name + "\" /></a>"
+<img class=\"pub-cover\" src=\"/themes/Mirage/images/recentlyIntegrated-" + cover_image + "\" alt=\"" + journal_name + "\" /></a>"
     return markup
 
-recent_oct2014 = (
-    ("Ecological Applications", "http://www.esajournals.org/loi/ecap", "ecoApp.png"),
-    ("Royal Society Open Science","http://royalsocietypublishing.org/royal-society-open-science","rsos.png"),
-)
+# via http://stackoverflow.com/a/3637103
+class DefaultHelpParser(argparse.ArgumentParser):
+    def error(self, message):
+        sys.stderr.write('error: %s\n' % message)
+        self.print_help()
+        sys.exit(2)
 
-for journal in plos_journals:
-    print render_itemsummary(journal[0], journal[1], journal[2])
+def main():
+    parser = DefaultHelpParser(
+        description='Generate Dryad XML markup for journal covers, using the provided name, link URL, and cover image',
+        epilog='Journal name and URL should be enclosed in double quotes.\nImage files should exist in both coverimages and recentlyIntegrated versions. Use resize_cover.py for those.',
+        add_help=True,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('-n', '--name', metavar='"Journal Name"', type=str, required=True)
+    parser.add_argument('-u', '--url', metavar='"http://website.com/"', type=str, required=True)
+    parser.add_argument('-i', '--image', metavar='"image-filename.png"', type=str, required=True)
 
-for journal in recent_oct2014:
-    print render_recentlyintegrated(journal[0], journal[2])
+    args = vars(parser.parse_args())
+    print "Paste into dspace/modules/xmlui/src/main/webapp/themes/Mirage/Mirage.xsl"
+    print "Update div#recently_integrated_journals, limit to 4"
+    print " Begin Recently Integrated ".center(80,'=')
+    print render_recentlyintegrated(args['name'], args['image'])
+    print " End Recently Integrated ".center(80,'=')
+    print ""
+    print "Paste alphabetically into dspace/modules/xmlui/src/main/webapp/themes/Mirage/DryadItemSummary.xsl"
+    print "Look for '<!-- this generates the linked journal image '"
+    print " BEGIN DryadItemSummary.xsl ".center(80,'=')
+    print render_itemsummary(args['name'], args['url'], args['image'])
+    print " END DryadItemSummary.xsl ".center(80,'=')
+
+if __name__ == '__main__':
+    main()
