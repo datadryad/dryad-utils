8ae50c04f5c9b8be121ff4e131bbee172f3453ed
daisie_local <daisieh@gmail.com>
2015-08-20 18:22:13 -0700

    doh, match on collection_id

--- process_hamr.py
+++ process_hamr.py
@@ -45,7 +45,7 @@ def perform_hamr_task(doi):
   with engine.connect() as conn:
     collection_id = [get_collection_id_for_doi(conn, doi)][0]
   #http://hdl.handle.net/10255/dryad.35555
-  match = re.search('(10255\/dryad\.\d+)', doi)
+  match = re.search('(10255\/dryad\.\d+)', collection_id)
   if match:
     collection_id = m.group(0)  
     print collection_id
