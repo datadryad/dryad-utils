7ac26345e60b053f7c052a3313ded29f62aab77c
Ryan Scherle <ryan@scherle.org>
2016-02-05 00:36:03 -0500

    Add script to find most downloads of a file

---
+++ spider-cleaner/statsMostDownloadsOfFile.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#
+# Takes one argument, which is the item ID of a data file object.
+# Returns a list of the 20 IP addresses that have downloaded this 
+# file the most, along with the number of downloads for each IP.
+
+curl -# "http://datadryad.org/solr/statistics/select/?indent=on&fl=ip&rows=10000000&q=-isBot:true+owningItem:$1" | grep ip | sort | uniq -c | sort -rn | head -20
+
