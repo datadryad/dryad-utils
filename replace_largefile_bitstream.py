4747d92c19febefdf01ea920a186f6b87e83d872
daisie_local <daisieh@gmail.com>
2016-02-17 14:41:02 -0800

    if there are no rows returned, the len of output is actually 2, not 1

--- replace_largefile_bitstream.py
+++ replace_largefile_bitstream.py
@@ -88,7 +88,7 @@ def dict_from_query(sql):
     # Now execute it
     cmd = "psql -A -U dryad_app dryad_repo -c \"%s\"" % sql
     output = [line.strip().split('|') for line in os.popen(cmd).readlines()]
-    if len(output) == 1:
+    if len(output) <= 2: # the output should have at least 3 lines: header, body rows, number of rows
         return None
     else:
         return dict(zip(output[0],output[1]))
