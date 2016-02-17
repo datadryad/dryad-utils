92ea15920b155a293935b866eb80efc3527d5398
Ryan Scherle <ryan@scherle.org>
2016-02-17 16:44:03 -0500

    Display usage if argument count is incorrect

--- spider-cleaner/statsMostDownloadsOfFile.sh
+++ spider-cleaner/statsMostDownloadsOfFile.sh
@@ -5,5 +5,10 @@
 # Returns a list of the 20 IP addresses that have downloaded this 
 # file the most, along with the number of downloads for each IP.
 
+if [ "$#" -ne 1 ]; then
+    echo "Usage: statsMostDownloadsOfFile.sh file_item_id"
+    exit
+fi
+
 curl -# "http://datadryad.org/solr/statistics/select/?indent=on&fl=ip&rows=10000000&q=-isBot:true+owningItem:$1" | grep ip | sort | uniq -c | sort -rn | head -20
 
