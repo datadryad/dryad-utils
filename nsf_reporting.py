61857022a711291e63e79e46facee0dff4387c05
daisie_local <daisieh@gmail.com>
2017-02-08 13:31:44 -0800

    test

--- nsf_reporting.py
+++ nsf_reporting.py
@@ -98,7 +98,7 @@ def main():
             print item_id
             sponsor_id = var_from_query("select sponsor_id from shoppingcart where item = %s" % (item_id),'sponsor_id')
             # don't print if it's a versioned item, which means there's no cart
-            version = False
+            version = True
             if sponsor_id is None:
                 version = True
                 
