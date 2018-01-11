8bf5045a7a299948c22b995dbb05bbf274e5ed7d
daisie_local <daisieh@gmail.com>
2018-01-10 19:19:28 -0800

    don't use env variable

--- transfer.sh
+++ transfer.sh
@@ -10,7 +10,7 @@ then
 	rsync -rltgDvz --no-p --no-g --chmod=ug=rwx,o=rx /dryad-data/transfer/ /dryad-data/transfer-complete
 	find /dryad-data/transfer -mindepth 2 -mmin +180 -exec rm -rf {} \;
 	echo "aws sync"
-	aws s3 sync /dryad-data/transfer-complete/ s3://$FTP_BUCKET/ --delete
+	aws s3 sync /dryad-data/transfer-complete/ s3://dryad-ftp/ --delete
 	python ~/dryad-utils/transfer_s3.py
 	rm ~/transfer_lock
 fi
