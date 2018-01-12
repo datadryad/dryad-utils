d8bed123153493cb18859cc0716715604d98b73b
daisie_local <daisieh@gmail.com>
2018-01-12 00:28:19 -0800

    full path, flush buffer

--- transfer_s3.py
+++ transfer_s3.py
@@ -4,6 +4,7 @@ __author__ = 'daisie'
 
 import os
 import re
+import sys
 import json
 import hashlib
 
@@ -13,14 +14,14 @@ TRANSFER_PATH = '/dryad-data/transfer-complete/'
 FTP_BUCKET = os.environ['FTP_BUCKET']
 
 def main():
-    cmd = 'aws s3 ls s3://%s/ --recursive' % (FTP_BUCKET)
+    cmd = '/usr/local/bin/aws s3 ls s3://%s/ --recursive' % (FTP_BUCKET)
     results = list(os.popen(cmd))
     for file in results:
         parts = re.split('\s+', file, 3)
         if (re.match("^\.", parts[3]) is None):
             key = parts[3].rstrip()
             print key
-            cmd = 'aws s3api head-object --bucket %s --key "%s"' % (FTP_BUCKET, key)
+            cmd = '/usr/local/bin/aws s3api head-object --bucket %s --key "%s"' % (FTP_BUCKET, key)
             metadata = json.load(os.popen(cmd))
             if ('md5' not in metadata['Metadata']):
                 print "  ...calculating and adding an md5 tag"
@@ -29,11 +30,12 @@ def main():
                     for chunk in iter(lambda: f.read(4096), ""):
                         hash.update(chunk)
                 md5 = hash.hexdigest()
-                cmd = 'aws s3 cp "s3://%s/%s" "s3://%s/%s" --metadata md5=%s' % (FTP_BUCKET, key, FTP_BUCKET, key, md5)
+                cmd = '/usr/local/bin/aws s3 cp "s3://%s/%s" "s3://%s/%s" --metadata md5=%s' % (FTP_BUCKET, key, FTP_BUCKET, key, md5)
                 os.popen(cmd)
                 print "  md5 tag of %s was added" % (md5)
             else:
                 print "  md5 tag is %s" % (metadata['Metadata']['md5'])
+        sys.stdout.flush()            
 
 if __name__ == '__main__':
     main()
