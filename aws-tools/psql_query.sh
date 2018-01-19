53912a3db3739973798d786a3d655cbc3d56893d
daisie_local <daisieh@gmail.com>
2018-01-18 22:51:18 -0800

    Ryan's postgres convenience script

---
+++ aws-tools/psql_query.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Query postgres in a way that allows easy processing of the results in a bash script
+psql --host=$DRYAD_DB_HOST \
+   --port=5432 \
+   --username=dryad_app  \
+   --dbname=dryad_repo \
+   -t \
+   --no-align \
+   --field-separator ' ' \
+   --quiet \
+   -c "$1"
