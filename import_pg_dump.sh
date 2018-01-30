ade54aa2c5509507b803f0165e1487e4d8bc76b0
daisie_local <daisieh@gmail.com>
2018-01-29 20:35:32 -0800

    Allow user to enter specific DB_HOST

--- import_pg_dump.sh
+++ import_pg_dump.sh
@@ -2,6 +2,7 @@
 #
 
 DUMPFILE=$1
+DBHOST=$2
 THREADS=4   # number of threads to spawn for update
 
 if [[ "$DUMPFILE" == "" ]]; then
@@ -16,16 +17,22 @@ if [[ ! -f "$DUMPFILE" ]]; then
     exit 1
 fi
 
-if [[ "$DRYAD_DB_HOST" == "" ]]; then
+if [[ "$DBHOST" == "" ]]; then
+	DBHOST=$DRYAD_DB_HOST
+    echo "Exiting."
+    exit 1;
+fi
+
+if [[ "$DBHOST" == "" ]]; then
 	psql -U dryad_app -d dryad_repo -c "select pg_terminate_backend(pid) from pg_stat_activity where datname='dryad_repo'"
 	dropdb dryad_repo
 	createdb -U dryad_app -E UNICODE dryad_repo
 	pg_restore -j $THREADS -d dryad_repo -U dryad_app $DUMPFILE 
 fi
 
-if [[ "$DRYAD_DB_HOST" != "" ]]; then
-	psql --host $DRYAD_DB_HOST -U dryad_app -d dryad_repo -c "select pg_terminate_backend(pid) from pg_stat_activity where datname='dryad_repo'"
-	dropdb --host $DRYAD_DB_HOST dryad_repo
-	createdb --host $DRYAD_DB_HOST -U dryad_app -E UNICODE dryad_repo
-	pg_restore --host $DRYAD_DB_HOST -j $THREADS -d dryad_repo -U dryad_app $DUMPFILE 
+if [[ "$DBHOST" != "" ]]; then
+	psql --host $DBHOST -U dryad_app -d dryad_repo -c "select pg_terminate_backend(pid) from pg_stat_activity where datname='dryad_repo'"
+	dropdb --host $DBHOST -U dryad_app dryad_repo
+	createdb --host $DBHOST -U dryad_app -E UNICODE dryad_repo
+	pg_restore --host $DBHOST -j $THREADS -d dryad_repo -U dryad_app $DUMPFILE 
 fi
