d6268c150630ad7fefbc37b62ff0dd386fd3d1e0
daisie_local <daisieh@gmail.com>
2018-02-14 15:33:32 -0800

    import the db too

--- import_recent_dryad.sh
+++ import_recent_dryad.sh
@@ -3,5 +3,7 @@
 
 echo "grabbing latest from aws..."
 aws s3 cp s3://dryad-backup/databaseBackups/dryadDBlatest.sql $HOME/dryadDBlatest.sql 
+import_pg_dump.sh $HOME/dryadDBlatest.sql
+
 mkdir -p /opt/dryad/solr
 aws s3 sync s3://dryad-backup/solrBackups/ /opt/dryad/solr --delete
