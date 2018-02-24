be1dda8683a491e7e75c860275352b884204a1dd
daisie_local <daisieh@gmail.com>
2018-02-23 17:50:19 -0800

    update script to work for different ports

--- ping-datadryad.sh
+++ ping-datadryad.sh
@@ -7,8 +7,15 @@ if [ -z "$1" ]
   HOST_TO_QUERY=$1
 fi
 
+if [ -z "$2" ]
+  then
+  PORT=8080
+  else
+  PORT=$2
+fi
+
 echo "querying datadryad.org to see if it is pointing at $HOST_TO_QUERY"
-while ping -c 1 datadryad.org | grep -q -m 2 $HOST_TO_QUERY 
+while nc -vz $HOST_TO_QUERY $PORT 2>&1 | grep -q succeeded
 do
     echo "datadryad.org still points at $HOST_TO_QUERY"
     sleep 10
