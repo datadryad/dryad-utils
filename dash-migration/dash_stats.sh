8baed2a5a6559628868a6acbffb033360aad8e02
Ryan Scherle <ryan@scherle.org>
2019-01-29 05:22:03 +0000

    add utility scripts

---
+++ dash-migration/dash_stats.sh
@@ -0,0 +1,11 @@
+# Usage: dash_stats.sh <handle-to-transfer>
+# If no handle is supplied, use the default of "the data packages collection"
+
+if [ -z "$1" ]
+then
+    HANDLE="10255/3"
+else
+    HANDLE="$1"
+fi
+
+/opt/dryad/bin/dspace curate -v -t dashstats -i $HANDLE -r - >dashStats.log
