5cbf0eb57cd99f89f55fa20e30460695a4d605ef
Ryan Scherle <ryan@scherle.org>
2019-03-25 03:54:46 +0000

    clean old values from script

--- dash-migration/dash_stats_make_list.sh
+++ dash-migration/dash_stats_make_list.sh
@@ -3,10 +3,7 @@
 # Script to generate a list of items to be transferred to DASH
 # Assumes the PGHOST and PGPASSWORD environment variables are set.
 
-echo Creating list of items to transfer to DASH...
-
-NUM_ITEMS=5000;
-DANS_METADATA_FIELD=162
+echo Creating list of items to transfer stats to DASH...
 
 psql -qt -U dryad_app -d dryad_repo -c "select handle from handle where resource_id in (select item_id from item where in_archive=TRUE and owning_collection=2);" -o items.txt
 
