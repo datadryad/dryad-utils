eca98125ca5f137e10cf920f1cfe11b3778bcc34
Ryan Scherle <ryan@scherle.org>
2019-04-19 15:16:32 +0000

    add scripts for processing bulk lists of items

--- dash-migration/make_handle_list_archived.sh
+++ dash-migration/make_handle_list_archived.sh
@@ -3,7 +3,7 @@
 # Script to generate a list of items to be transferred to DASH
 # Assumes the PGHOST and PGPASSWORD environment variables are set.
 
-echo Creating list of items to transfer stats to DASH...
+echo Creating list of items to transfer to DASH...
 
 psql -qt -U dryad_app -d dryad_repo -c "select handle from handle where resource_id in (select item_id from item where in_archive=TRUE and owning_collection=2);" -o items.txt
 
---
+++ dash-migration/make_handle_list_workspace.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Script to generate a list of items to be transferred to DASH
+# Assumes the PGHOST and PGPASSWORD environment variables are set.
+
+echo Creating list of items to transfer to DASH...
+
+psql -qt -U dryad_app -d dryad_repo -c "select handle from handle where resource_id in (select item_id from workspaceitem where collection_id=2);" -o items.txt
+
+
+
+echo List of items is in items.txt
